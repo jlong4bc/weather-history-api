d39f36bc9c2e8903613b68d4a77d7c7d240e131b
jlong4bc <jlong4bc@hotmail.com>
2026-03-22 21:32:23 -0400

    Initial Commit

---
+++ src/main/java/com/github/jlong4bc/weatherhistoryapi/WeatherHistoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.jlong4bc.weatherhistoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherHistoryApiApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(WeatherHistoryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jlong4bc/weatherhistoryapi/WeatherHistoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jlong4bc.weatherhistoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherHistoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
